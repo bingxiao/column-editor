f08846805e6b72fd347fbf61c978d06748f2ff04
bingxiao <alberthwc2003@gmail.com>
2012-02-18 17:47:00 +0800

    Rename with hjoin, hcut

---
+++ hcut.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# In the future:
+# 1. consider multiple files. Joinning files with the same rows.
+# 2. consider parsing standard input.
+
+
+import sys, os
+
+argv = sys.argv[ 1 ]
+fn, columns_str = argv.rsplit( '/', 1 )
+
+# print argv
+# print fn, columns_str
+
+if not os.path.isfile( fn ):
+    print >>sys.stderr, 'File does not exist: <%s>'%fn
+
+def parse_segment( columns_str, columns_max ):
+    NF = columns_max                    # 1 based.
+    stack = []
+    for segment in columns_str.split( ',' ):
+        if '-' in segment:
+            a, b = segment.split( '-' )
+            a = int( a ) if a != '' else 1  
+            b = int( b ) if b != '' else NF
+            stack.extend( range( a-1, b ) )
+        else:
+            if segment != '':
+                stack.append( int( segment )-1 )
+    # print stack                             # note that after parsing, numbers are converted to 0-based.
+    return stack
+
+
+flag_diffField = False
+with open( fn ) as handler:
+    segment_stack = None
+    for line in handler:
+        if line.startswith( '#' ) or not line.strip():
+            print line.strip( '\n\r' )  # the same as previous.
+        items = line.strip( '\n\r' ).split( '\t' )
+        if segment_stack == None:
+            max_col = len( items )
+            segment_stack = parse_segment( columns_str, max_col )
+        else:                           # just check column numbers are the same.
+            if max_col != len( items ):
+                flag_diffField = True
+        rearranged_items = [ items[i] for i in segment_stack ]
+        print '\t'.join( rearranged_items )
+        
+if flag_diffField:
+    print >>sys.stderr,  "NF consistency. Numbers of columns for <%s> should be: <%d>"%( fn, max_col )
