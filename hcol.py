6c5eb08b1b8e3e19f6a9a8732cbec492bced2a2c
bingxiao <alberthwc2003@gmail.com>
2012-02-22 14:02:58 +0800

    add support for command line pipes.

--- hcol.py
+++ hcol.py
@@ -3,19 +3,34 @@
 # just show file columns by numbering.
 
 import sys
-argv = sys.argv[ 1 ]
 
-fn = argv
+
+
+if not sys.stdin.isatty():
+    if len( sys.argv ) > 1: print >>sys.stderr, 'Note that hcol ONLY handle <stdin> or only 1 file at once!'
+    handler = sys.stdin
+elif sys.argv[ 1 ] == '-':
+    if len( sys.argv ) > 2: print >>sys.stderr, 'Note that hcol ONLY handle <stdin> or only 1 file at once!'
+    handler = sys.stdin
+elif not os.path.isfile( fn ):
+    if len( sys.argv ) > 2: print >>sys.stderr, 'Note that hcol ONLY handle <stdin> or only 1 file at once!'
+    argv = sys.argv[ 1 ]
+    fn = argv
+    print >>sys.stderr, 'File does not exist: <%s>'%fn
+    exit( 0 )
+    handler = open( fn )
 
 table = []
-with open( fn ) as handler:
-    for n, line in enumerate( handler ):
-        if line.startswith( '#' ) or not line.strip():
-            continue
-        items = line.strip( '\n\r' ).split( '\t' )
-        table.append( items )
-        if n == 5:                      # only view first <n> lines.
-            break
+
+for n, line in enumerate( handler ):
+    if line.startswith( '#' ) or not line.strip():
+        continue
+    items = line.strip( '\n\r' ).split( '\t' )
+    table.append( items )
+    if n == 5:                      # only view first <n> lines.
+        break
+
+handler.close()
 
 ncol = len( table[ 0 ] )
 for n in xrange( ncol ):
--- hjoin.py
+++ hjoin.py
@@ -3,42 +3,66 @@
 
 import sys
 from optparse import OptionParser
-usage = 'Usage: $0 -a # -b # [file 1] [file 2] \nAlways deliminate by TAB.'
+usage = '''
+Method 1: hjoin.py -a [n1] -b [n2] [file1] [file2]
+Method 2: hjoin.py [file1]/[n1] [file2]/[n2]
+Note: the input files must ALWAYS be deliminated by TABs.
+'''
+
 parser = OptionParser( usage )
-parser.add_option( "-a", "--a", dest='col1', help='a number, which column in file 1' )
-parser.add_option( "-b", "--b", dest='col2', help='a number, which column in file 2' )
-(options, args) = parser.parse_args()
+parser.add_option( "-a", "--a", dest='col1', help='by which column in file 1. A number' )
+parser.add_option( "-b", "--b", dest='col2', help='by which column in file 2. A number' )
+( options, args ) = parser.parse_args()
 
-
-if len( args ) !=2 or not (options.col1 and options.col2):
-    print usage
-    print 'Illegal arguments.'
-    print "opts:", options
-    print "args:", args
+try:
+    if options.col1 == None and options.col2 == None:
+        a, b = args[:2]
+        in_file, col1 = a.rsplit( '/', 1 )
+        db_file, col2 = b.rsplit( '/', 1 )
+        col1, col2 = int( col1 ), int( col2 )
+    elif len( args ) !=2 or not (options.col1 and options.col2):
+        print >>sys.stderr, options.col1, options.col2
+        print >>sys.stderr, 'Illegal arguments.' 
+        exit( 1 )
+    else:
+        in_file, db_file = args[:2]
+        col2 = int( options.col2 )
+        col1 = int( options.col1 )
+except:
+    print >>sys.stderr, 'Parsing argument failed.'
+    parser.print_help()
     exit( 0 )
-
-in_file, db_file = args[:2]
+    
 flag_dup = False
-
 db = {}
-col2 = int( options.col2 )
-with open( db_file ) as handler:
-    for line in handler:
-        if line.startswith( '#' ): continue
-        items = line.strip( '\n\r' ).split( '\t' )
-        key = items[ col2 - 1 ]
-        if key in db:
-            flag_dup = True
-        db[ key ] = items
 
-col1 = int( options.col1 )
-with open( in_file ) as handler:
-    for line in handler:
-        if line.startswith( '#' ): continue
-        items = line.strip( '\n\r' ).split( '\t' )
-        key = items[ col1 - 1 ]
-        if key in db:
-            print '\t'.join( items + db[ key ] )
+if db_file == '-': handler = sys.stdin
+else: handler = open( db_file )
+
+for line in handler:
+    if line.startswith( '#' ): continue
+    items = line.strip( '\n\r' ).split( '\t' )
+    key = items[ col2 - 1 ]
+    if key in db:
+        flag_dup = True
+    db[ key ] = items
+
+handler.close()
+
+
+if in_file == '-': handler = sys.stdin
+else: handler = open( in_file )
+
+for line in handler:
+    if line.startswith( '#' ): continue
+    items = line.strip( '\n\r' ).split( '\t' )
+    key = items[ col1 - 1 ]
+    if key in db:
+        print '\t'.join( items + db[ key ] )
+
+handler.close()
+
 
 if flag_dup:
-    print >>sys.stderr, '## ALERT( Duplications in db_file:<%s>. Using the last value. )'%db_file
+    print >>sys.stderr, '## ALERT( At least one duplication in db_file:<%s>. The last value was taken. )'%db_file
+
